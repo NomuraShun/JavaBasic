b2de1d58623473f9f663973a3f1b2268a06e9432
Nomurashun <shun.nomura.likeit@gmail.com>
2019-01-28 16:19:23 +0900

    12-1

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,7 +10,7 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-		Book[]
+		Book[] B;  
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 		String input = new java.util.Scanner(System.in).nextLine();
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
